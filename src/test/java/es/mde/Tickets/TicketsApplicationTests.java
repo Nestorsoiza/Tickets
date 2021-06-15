c68448677f956f92778a7cfbfdbdc65ead8b56a8
Nestor <nestor.soiza83@gmail.com>
2021-06-15 21:09:33 +0200

    Haciendo cambios

--- src/test/java/es/mde/Tickets/TicketsApplicationTests.java
+++ src/test/java/es/mde/Tickets/TicketsApplicationTests.java
@@ -1,12 +1,10 @@
 package es.mde.Tickets;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TicketsApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
